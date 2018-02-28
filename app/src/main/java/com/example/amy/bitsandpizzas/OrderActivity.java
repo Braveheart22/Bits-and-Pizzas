0b428347fe247946778fd44a3ea016db9a72186f
John Strack <Braveheart22@GMail.com>
2018-02-28 09:11:48 -0500

    Add up button to order activity

--- app/src/main/java/com/example/amy/bitsandpizzas/OrderActivity.java
+++ app/src/main/java/com/example/amy/bitsandpizzas/OrderActivity.java
@@ -1,5 +1,6 @@
 package com.example.amy.bitsandpizzas;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -13,5 +14,9 @@ public class OrderActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (getSupportActionBar() != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
